fix incorrect dim of cond for code2wav

vllm/model_executor/models/qwen_2_5_omni.py
@@ -442,7 +442,7 @@ class Qwen2_5OmniForConditionalGeneration(nn.Module, SupportsMultiModal,
         # Fallback: create dummy cond/ref_mel if not provided
         token2wav_dev = self._module_device(self.token2wav)
         if cond is None:
-            cond = torch.zeros((1, 300, 80), device=token2wav_dev, dtype=torch.float32)
+            cond = torch.zeros((300, 80), device=token2wav_dev, dtype=torch.float32)
         if ref_mel is None:
             ref_mel = torch.zeros((1, 300, 80), device=token2wav_dev, dtype=torch.float32)
         # Ensure codec is long